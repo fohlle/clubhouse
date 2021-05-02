3e01d3ad56f123f2c7e64982915f08625e1cfb98
fohlle <patte2000@hotmail.com>
2021-05-02 19:17:15 +0200

    some styleing

---
+++ app/models/member.rb
@@ -0,0 +1,10 @@
+class Member < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+
+  validates :username, presence: true
+end
---
+++ db/migrate/20210501170104_add_column_to_member.rb
@@ -0,0 +1,6 @@
+class AddColumnToMember < ActiveRecord::Migration[6.1]
+  def change
+    add_column :members, :username, :string
+    add_index :members, :username, unique: true
+  end
+end
